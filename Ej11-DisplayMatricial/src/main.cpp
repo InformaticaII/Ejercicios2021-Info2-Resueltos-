3741573967270503b09a9587321a93100aef5ff8
Fernando Daniele <fernandodaniele1993@gmail.com>
2021-04-27 21:01:19 -0300

    Actualizo ej 11

--- Ej11-DisplayMatricial/src/main.cpp
+++ Ej11-DisplayMatricial/src/main.cpp
@@ -2,20 +2,21 @@
 
 void setup()
 {
-  DDRD= DDRD | B01111111; // puerto D0-D3 como salida.
-  DDRB= DDRB | B00011111; // puerto B0-B1 como salida.
+  //para display de 7x5
+  DDRD= DDRD | B01111111; // puerto D0-D6 como salida. -- Filas
+  DDRB= DDRB | B00011111; // puerto B0-B4 como salida. -- Columnas
 }
             
 byte fila, columna;
 
 void loop()
 {
-  for (fila = B00000001;fila <= B00010000; fila = fila<<1)
+  for (columna = B00000001;columna <= B00010000; columna = columna<<1)
   {
-    PORTB=fila; 
-    for(columna = B0000001;columna <= B01000000; columna = columna<<1) 
+    PORTB=columna; 
+    for(fila = B0000001;fila <= B01000000; fila = fila<<1) 
     {
-      PORTD=~columna;
+      PORTD=~fila;
       delay(500);
     }
   }
